9479009c657f65a10f724e572ef8fd2aaeb38963
notABluesSinger <rob@whatagoodbot.com>
2022-10-22 14:56:34 +0100

    Context on quotes

--- src/services/thedonald.js
+++ src/services/thedonald.js
@@ -3,5 +3,5 @@ import { buildUrl, makeRequest } from '../utils/networking.js'
 export default async () => {
   const url = buildUrl('api.whatdoestrumpthink.com/api/v1/quotes/random')
   const response = await makeRequest(url)
-  return { payload: { message: response?.message } }
+  return { payload: { message: `Donald once said: ${response?.message}` } }
 }
