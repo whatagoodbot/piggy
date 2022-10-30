ca57eca4ce36f6db21601d7416215d673b7dcb8d
notABluesSinger <rob@whatagoodbot.com>
2022-10-30 14:05:25 +0000

    Switching to RPC

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,10 +1,16 @@
 import search from 'youtube-search'
+import { getString } from '../libs/grpc.js'
 
 export default async (payload) => {
   return new Promise((resolve, reject) => {
     search(`${payload.nowPlaying.title} by ${payload.nowPlaying.artist}`, { maxResults: 1, key: process.env.YOUTUBE_API_KEY }, async (err, results) => {
       if (err) {
-        return reject(err)
+        const string = await getString('youtubeError')
+        return resolve({
+          payload: {
+            message: string.value
+          }
+        })
       }
       const reply = { payload: { message: results[0].link } }
       if (results[0].thumbnails.medium.url) reply.payload.image = results[0].thumbnails.medium.url
