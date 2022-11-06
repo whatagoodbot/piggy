db6be1c849f4aaa318cef389f4ff300eeddc2e94
Nicola Bosco <nicola.bosco87@gmail.com>
2022-11-06 18:08:51 +0100

    Add Urban command

--- test/passing.test.js
+++ test/passing.test.js
@@ -1,7 +1,16 @@
 import { expect } from 'chai'
+import urban from '../src/services/urban.js'
 
 describe('networking', function () {
   it('should return a function to add text responder', () => {
     expect('').to.be.a('string')
+  })
+})
+
+describe('Commands tests', function () {
+  this.timeout(5000)
+  it('should handle urban command', async () => {
+    const response = await urban({ arguments: 'hava' })
+    expect(response).to.have.nested.property('payload.message')
   })
 })
