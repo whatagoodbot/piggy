02175e6918de577bf140bbc369f2bd1f60749e21
notABluesSinger <rob@whatagoodbot.com>
2022-11-26 09:09:58 +0000

    Add latest to available commands

--- src/services/index.js
+++ src/services/index.js
@@ -1,24 +1,27 @@
-import dadjoke from './dadjoke.js'
-import giphy from './giphy.js'
-import weather from './weather.js'
-import yt from './youtube.js'
-import randomcoffee from './randomcoffee.js'
 import cat from './cat.js'
+import compliment from './compliment.js'
 import corporatebs from './corporatebs.js'
+import dadjoke from './dadjoke.js'
 import dog from './dog.js'
 import fact from './fact.js'
 import fortune from './fortune.js'
 import fox from './fox.js'
 import geekjoke from './geekjoke.js'
+import giphy from './giphy.js'
 import inspire from './inspirobot.js'
 import kanye from './kanye.js'
 import meowfact from './meowfact.js'
+import randomcoffee from './randomcoffee.js'
 import thedonald from './thedonald.js'
 import thatreally from './trbmb.js'
 import urban from './urban.js'
+import weather from './weather.js'
+import yt from './youtube.js'
+import zen from './inspirational.js'
 
 export default {
   cat,
+  compliment,
   corporatebs,
   dadjoke,
   dog,
@@ -35,5 +38,6 @@ export default {
   thatreally,
   thedonald,
   weather,
-  yt
+  yt,
+  zen
 }
